b4cc474500e1eb0c6e32fbd343b51451acdacdd2
SuryaRaavi <sryraavi.adiputra@gmail.com>
2024-05-19 09:24:25 +0700

    Penyesuaian

--- playlist/urls.py
+++ playlist/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('playlist_detail/<str:id_playlist>', playlist_detail, name='playlist_detail'),
   path('add_playlist/', add_playlist, name='add_playlist'),
   path('delete_playlist/<str:id_playlist>', delete_playlist, name="delete_playlist"),
-  path('add_song/<str:id_playlist>', add_song, name="add_song")
+  path('add_song/<str:id_playlist>', add_song, name="add_song"),
+  path('playlist_detail/delete_song/<str:id_playlist>/<str:id_song>', delete_song, name="delete_song")
 ]
